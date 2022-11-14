f6f3ed4681cf4f8d0365c9d355e09fc0e2482799
Chris Titus <dfm.titus@gmail.com>
2022-11-14 12:13:30 -0600

    ubuntu-titus initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ubuntu-titus
+Ubuntu customizations from Chris Titus Tech
+ 
+## Requirements
+_This is a Ubuntu server build_
+
+### Download Ubuntu Server
+
+Download from <https://ubuntu.com/download/server> and install the ISO using any imaging tool. 
+
+### Installation Guide
+
+```
+git clone https://github.com/ChrisTitusTech/ubuntu-titus
+cd ubuntu-titus
+sudo ./install.sh
+```
+
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Making .config and Moving config files and background to Pictures
+cd "$builddir" || exit
+mkdir -p "/home/$username/.config"
+mkdir -p "/home/$username/.fonts"
+mkdir -p "/home/$username/Pictures"
+mkdir -p /usr/share/sddm/themes
+cp .Xresources "/home/$username"
+cp .Xnord "/home/$username"
+cp -R dotconfig/* "/home/$username/.config/"
+cp bg.jpg "/home/$username/Pictures/"
+chown -R "$username:$username" "/home/$username"
+
+# Installing sugar-candy dependencies
+nala install libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
+# Installing Essential Programs 
+nala install feh bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pavucontrol -y
+# Installing Other less important Programs
+nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji sddm variety -y
+
+# Download Nordic Theme
+cd /usr/share/themes/ || exit
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd "$builddir" || exit
+nala install fonts-font-awesome
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d "/home/$username/.fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d "/home/$username/.fonts"
+mv dotfonts/fontawesome/otfs/*.otf "/home/$username/.fonts/"
+chown "$username:$username" "/home/$username/.fonts/*"
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors || exit
+./install.sh
+cd "$builddir" || exit
+rm -rf Nordzy-cursors
+
+# Install brave-browser
+sudo nala install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo nala update
+sudo nala install brave-browser -y
+
+# Enable graphical login and change target from CLI to GUI
+tar -xzvf slice.tar.gz -C /usr/share/sddm/themes
+cp -f "$builddir/sddm.conf" /etc/
+systemctl enable sddm
+systemctl set-default graphical.target
